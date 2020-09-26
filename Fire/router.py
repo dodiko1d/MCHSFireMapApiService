727e563efe9b49e658248cb11b4df6ae4ee58a2e
AntonKAb <anton_10_1999@mail.ru>
2020-09-26 21:01:46 +0700

    Server

--- Fire/router.py
+++ Fire/router.py
@@ -15,95 +15,38 @@ async def get_db():
 router = APIRouter()
 
 
+async def check_firepoint(fire_id: int, db: Session = Depends(get_db)):
+    db_fire = controller.get_firepoint_by_id(db, fire_id=fire_id)
+    if not db_fire:
+        raise HTTPException(status_code=400, detail='Firepoint with this id is not exist.')
 
 
-async def check_product_existence(product_id: int, db: Session = Depends(get_db)):
-    db_product = controller.get_product_data_by_id(db, product_id=product_id)
-    if not db_product:
-        raise HTTPException(status_code=400, detail='Product with this id does not exist.')
-
-
-@router.post('/create/', summary='Create your product.')
-async def create(product: schemas.ProductCreation, db: Session = Depends(get_db)):
-    db_product = controller.get_product_data_by_id(db, product_id=product.id)
-    if db_product:
-        raise HTTPException(status_code=400, detail='Product has been already registered.')
-    controller.create_product(db=db, product=product)
+@router.post('/add_new_fire/', summary='Add new firepoint.')
+async def add_new_fire(fire: schemas.FireSpot, db: Session = Depends(get_db)):
+    db_fire = controller.get_firepoint_by_id(db, fire_id=fire.id)
+    if db_fire:
+        raise HTTPException(status_code=400, detail='Fire has been already added.')
+    controller.fire_report(db=db, fire=fire)
     return {'status_code': '200'}
 
 
 @router.post(
-    '/remove/{product_id}',
-    summary='Remove a product.',
-    dependencies=[Depends(check_product_existence)]
+    '/remove_fire/{fire_id}',
+    summary='Remove a firepoint.',
+    dependencies=[Depends(check_firepoint)]
 )
-async def remove(product_id: int, db: Session = Depends(get_db)):
-    controller.remove_product(db=db, product_id=product_id)
-    return {'status_code': '200'}
+async def remove_fire(fire_id: int, db: Session = Depends(get_db)):
+    return controller.fire_out(db=db, fire_id=fire_id)
+    # return {'status': 'Deleted'}
 
 
 @router.post(
-    '/change_property/{product_id}/{property_name}/{new_property_value}',
-    summary='Change product property.',
-    dependencies=[Depends(check_product_existence)]
+    '/precicpitation_start/',
+    summary='Precipitation added'
 )
-async def change_product_property(product_id: int, property_name: str, new_property_value, db: Session = Depends(get_db)):
-    return controller.change_product_property(db=db, product_id=product_id,
-                                              property_name=property_name,
-                                              new_property_value=new_property_value)
+async def precipitation_start(fire_id: int, volume: int, db: Session = Depends(get_db)):
+    return controller.precipitation_start(db=db, fire_id=fire_id, volume=volume)
 
 
-@router.post(
-    '/increase_stock_balance/{product_id}/{increasing_value}',
-    summary='Increase product stock balance.',
-    dependencies=[Depends(check_product_existence)]
-)
-def increase_stock_balance(product_id: int, increasing_value: int, db: Session = Depends(get_db)):
-    return controller.increase_stock_balance(db=db, product_id=product_id, increasing_value=increasing_value)
-
-
-@router.post(
-    '/reduce_stock_balance/{product_id}/{reducing_value}',
-    summary='Reduce product stock balance.',
-    dependencies=[Depends(check_product_existence)]
-)
-def reduce_stock_balance(product_id: int, reducing_value: int, db: Session = Depends(get_db)):
-    return controller.reduce_stock_balance(db=db, product_id=product_id, reducing_value=reducing_value)
-
-
-@router.get(
-    '/get_rest_not_reserved_product/{product_id}',
-    summary='Get rest not reserved product amount.',
-    dependencies=[Depends(check_product_existence)]
-)
-def get_rest_not_reserved_product(product_id: int, db: Session = Depends(get_db)):
-    return controller.get_rest_not_reserved_product(db=db, product_id=product_id)
-
-
-@router.post(
-    '/increase_reserved_product/{product_id}/{increasing_value}',
-    summary='Increase reserved product.',
-    dependencies=[Depends(check_product_existence)]
-)
-def increase_reserved_product(product_id: int, increasing_value: int, db: Session = Depends(get_db)):
-    controller.increase_reserved_product(db=db, product_id=product_id, increasing_value=increasing_value)
-
-
-@router.post(
-    '/reduce_reserved_product/{product_id}/{reducing_value}',
-    summary='Reduce reserved product.',
-    dependencies=[Depends(check_product_existence)]
-)
-def reduce_reserved_product(product_id: int, reducing_value: int, db: Session = Depends(get_db)):
-    return controller.reduce_reserved_product(db=db, product_id=product_id, reducing_value=reducing_value)
-
-
-@router.post(
-    '/reduce_reserved_product/{product_id}/{new_group_id}',
-    summary='Change product group.',
-    dependencies=[Depends(check_product_existence)]
-)
-def change_product_group(product_id: int, new_group_id: int, db: Session = Depends(get_db)):
-    return controller.change_product_group(db=db, product_id=product_id, new_group_id=new_group_id)
 
 
