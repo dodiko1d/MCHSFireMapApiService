87f78a049db3116c25f40f7287b9287bea3c7c2d
AntonKAb <anton_10_1999@mail.ru>
2020-09-26 21:13:02 +0700

    Update function's names.

--- Fire/router.py
+++ Fire/router.py
@@ -16,14 +16,14 @@ router = APIRouter()
 
 
 async def check_firepoint(fire_id: int, db: Session = Depends(get_db)):
-    db_fire = controller.get_firepoint_by_id(db, fire_id=fire_id)
+    db_fire = controller.__get_firepoint_by_id(db, fire_id=fire_id)
     if not db_fire:
         raise HTTPException(status_code=400, detail='Firepoint with this id is not exist.')
 
 
 @router.post('/add_new_fire/', summary='Add new firepoint.')
 async def add_new_fire(fire: schemas.FireSpot, db: Session = Depends(get_db)):
-    db_fire = controller.get_firepoint_by_id(db, fire_id=fire.id)
+    db_fire = controller.__get_firepoint_by_id(db, fire_id=fire.id)
     if db_fire:
         raise HTTPException(status_code=400, detail='Fire has been already added.')
     controller.fire_report(db=db, fire=fire)
