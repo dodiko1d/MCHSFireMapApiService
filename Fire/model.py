d9a8f231e661130562eeb513b4cb33608fbc00cd
SergeyLankevich <43179675+SergeyLankevich@users.noreply.github.com>
2020-09-26 19:35:27 +0700

    Initital commit

--- Fire/model.py
+++ Fire/model.py
@@ -1,6 +1,6 @@
-""" Database Product Model. """
+""" Database Fire point Model. """
 
-from sqlalchemy import Column, ForeignKey, Integer, String, CheckConstraint
+from sqlalchemy import Column, ForeignKey, Integer, String, CheckConstraint, Float, Boolean
 from geoalchemy2 import Geography
 from database import Base
 from sqlalchemy.orm import relationship
@@ -11,5 +11,15 @@ class FirePoint(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     coordinates = Column(Geography(geometry_type='POINT'))
-    land_owner = Column(String)
+    owner = Column(String)
     land_type = Column(String)
+    fuel = Column(String)
+    wind_direction = Column(String)
+    wind_speed = Column(Float)
+    wind_gust = Column(Float)
+    humidity = Column(Float)
+    precipitation = Column(Float)
+    description = Column(String)
+    area = Column(Float)
+    status = Column(Boolean)
+    update_time = Column()
