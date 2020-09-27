a08c9128ad7a137702d039095908a6dc1b1efb3f
AntonKAb <anton_10_1999@mail.ru>
2020-09-27 09:22:59 +0700

    Add new functions.

--- Fire/router.py
+++ Fire/router.py
@@ -16,14 +16,14 @@ router = APIRouter()
 
 
 async def check_firepoint(fire_id: int, db: Session = Depends(get_db)):
-    db_fire = controller.get_firepoint_by_id(db, fire_id=fire_id)
+    db_fire = controller.__get_firepoint_by_id(db, fire_id=fire_id)
     if not db_fire:
         raise HTTPException(status_code=400, detail='Firepoint with this id is not exist.')
 
 
 @router.post('/add_new_fire/', summary='Add new firepoint.')
 async def add_new_fire(fire: schemas.FireSpot, db: Session = Depends(get_db)):
-    db_fire = controller.get_firepoint_by_id(db, fire_id=fire.id)
+    db_fire = controller.__get_firepoint_by_id(db, fire_id=fire.id)
     if db_fire:
         raise HTTPException(status_code=400, detail='Fire has been already added.')
     controller.fire_report(db=db, fire=fire)
@@ -46,6 +46,20 @@ async def remove_fire(fire_id: int, db: Session = Depends(get_db)):
 )
 async def precipitation_start(fire_id: int, volume: int, db: Session = Depends(get_db)):
     return controller.precipitation_start(db=db, fire_id=fire_id, volume=volume)
+
+
+@router.post(
+    '/precicpitation_stop/'
+)
+async def precipitation_stop(fire_id: int, db: Session = Depends(get_db)):
+    return controller.precipitation_stop(db=db, fire_id=fire_id)
+
+
+@router.post(
+    '/description/',
+)
+async def add_description(description: str, fire_id: int, db: Session = Depends(get_db)):
+    return controller.add_description(db=db, fire_id=fire_id, description=description)
 
 
 
